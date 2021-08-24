afc85f07a2b968d92cc353cc69a490c1cad13078
b3aaa <rodrlehqk@naver.com>
2021-08-24 20:16:22 +0900

    소수 찾기

---
+++ Baekjoon_1978.java
@@ -0,0 +1,35 @@
+package upload;
+
+import java.util.*;
+
+/*
+ * 소수 찾기
+ * 내가쓴 코드
+ * 결과 = 맞았습니다!!
+ */
+public class Baekjoon_1978 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.nextLine();
+		StringTokenizer st = new StringTokenizer(sc.nextLine()," ");
+		int c = 0;
+		while(st.hasMoreTokens()) {
+			int t = Integer.valueOf(st.nextToken());
+			if(t!=1) {
+				c++;
+			}else {
+				continue;
+			}
+			for(int i=2;i*i<=t;i++) {
+				if(t%i==0) {
+					c--;
+					break;
+				}
+			}
+		}
+		System.out.println(c);
+	}
+
+}
