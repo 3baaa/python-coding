c8dbdd4573c3016e25d6c52a89e09d21de05f418
b3aaa <rodrlehqk@naver.com>
2021-09-10 17:14:08 +0900

    퇴사(작성중2)

--- Baekjoon_14501_w2.java
+++ Baekjoon_14501_w2.java
@@ -7,7 +7,14 @@ import java.util.*;
  * 내가쓴 코드
  * 
  */
-public class Baekjoon_14501_w2 {
+import java.util.*;
+
+/*
+ * 퇴사
+ * 내가쓴 코드
+ * 
+ */
+public class Baekjoon_14501_w2{
 
 	static int n;
 	static int[] t,p;
@@ -27,11 +34,8 @@ public class Baekjoon_14501_w2 {
 	}
 	
 	public static int test(int s,int c,int v) {
-		System.out.println("-------------s= "+s+" v= "+v);
 		
 		if(s>=n) {
-			System.out.println("e");
-			System.out.println();
 			return v;
 		}
 		
@@ -40,26 +44,9 @@ public class Baekjoon_14501_w2 {
 			if(i+t[i]>n) {
 				continue;
 			}
-			System.out.println("s= "+s+" i= "+i+" v = "+v+" p["+i+"]= "+p[i]);
-			//v+=p[i];
-			System.out.println("v= "+v+" "+i+"+"+"t["+i+"] = "+(i+t[i]));
 			int a = test(i+t[i],c+1,v+p[i]);
-			System.out.println("a = "+a);
 			r=Math.max(r, a);
-			//v-=p[i];
-			System.out.println("vvvvv= "+v);
 		}
 		return r;
 	}
 }
-/*
-class B_14501 {
-	int v;
-	int c;
-	
-	public B_14501(int v,int c){
-		this.v = v;
-		this.c = c;
-	}
-}
-*/
