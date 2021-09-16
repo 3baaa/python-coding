2e4e3ad8f0e9d6b8b74420419b310cecd14c39f3
b3aaa <rodrlehqk@naver.com>
2021-09-16 16:23:40 +0900

    배열 돌리기 3

---
+++ Baekjoon_16935.java
@@ -0,0 +1,139 @@
+package upload.new_upload;
+
+/*
+ * 배열 돌리기 3
+ * 내가쓴 코드
+ * 결과 = 맞았습니다!!
+ */
+import java.util.*;
+
+public class Baekjoon_16935 {
+
+	static int n,m;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		m=sc.nextInt();
+		int r=sc.nextInt();
+		int[][] arr = new int[n][m];
+		int[] oper = new int[r];
+		sc.nextLine();
+		for(int i=0;i<n;i++) {
+			StringTokenizer st = new StringTokenizer(sc.nextLine()," ");
+			for(int j=0;st.hasMoreTokens();j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		StringTokenizer st = new StringTokenizer(sc.nextLine()," ");
+		for(int i=0;st.hasMoreTokens();i++) {
+			oper[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i : oper) {
+			arr=check_oper(i,arr);
+		}
+		arr_p(arr);
+	}
+	
+	public static void arr_p(int[][] arr) {
+		for(int[] n1 : arr) {
+			for(int n2 : n1) {
+				System.out.print(n2+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static int[][] check_oper(int i,int[][] arr) {
+		
+		switch(i) {
+		case 1 : arr=oper_1(arr); break;
+		case 2 : arr=oper_2(arr); break;
+		case 3 : arr=oper_3(arr); break;
+		case 4 : arr=oper_4(arr); break;
+		case 5 : arr=oper_5(arr); break;
+		case 6 : arr=oper_6(arr); break;
+		}
+		return arr;
+	}
+	
+	public static int[][] oper_1(int[][] arr) {
+		for(int s=0,e=n-1;s<e;s++,e--) {
+			int[] t = arr[s];
+			arr[s] = arr[e];
+			arr[e] = t;
+		}
+		return arr;
+	}
+	public static int[][] oper_2(int[][] arr) {
+		for(int s=0,e=m-1;s<e;s++,e--) {
+			for(int i=0;i<n;i++) {
+				int t = arr[i][s];
+				arr[i][s] = arr[i][e];
+				arr[i][e] = t;
+			}
+		}
+		return arr;
+	}
+	public static int[][] oper_3(int[][] arr) {
+		int[][] new_arr = new int[m][n];
+		for(int i=0;i<m;i++) {
+			int[] tarr = new int[n];
+			for(int j=0;j<n;j++) {
+				tarr[n-j-1] = arr[j][i];
+			}
+			new_arr[i] = tarr;
+		}
+		int t=m;
+		m=n;
+		n=t;
+		return new_arr;
+	}
+	public static int[][] oper_4(int[][] arr) {
+		int[][] new_arr = new int[m][n];
+		for(int i=0;i<m;i++) {
+			int[] tarr = new int[n];
+			for(int j=0;j<n;j++) {
+				tarr[j]=arr[j][m-i-1];
+			}
+			new_arr[i]=tarr;
+		}
+		int t=m;
+		m=n;
+		n=t;
+		return new_arr;
+	}
+	public static int[][] oper_5(int[][] arr) {
+		int[][] new_arr = new int[n][m];
+		int ch_n=n/2,ch_m=m/2;
+		
+		for(int i=0;i<ch_n;i++) {
+			for(int j=0;j<ch_m;j++) {
+				new_arr[i][j]=arr[i+ch_n][j];
+				new_arr[i+ch_n][j+ch_m]=arr[i][j+ch_m];
+			}
+			for(int j=ch_m;j<m;j++) {
+				new_arr[i][j]=arr[i][j-ch_m];
+				new_arr[i+ch_n][j-ch_m]=arr[i+ch_n][j];
+			}
+		}
+		
+		return new_arr;
+	}
+	public static int[][] oper_6(int[][] arr) {
+		int[][] new_arr = new int[n][m];
+		int ch_n=n/2,ch_m=m/2;
+		
+		for(int i=0;i<ch_n;i++) {
+			for(int j=0;j<ch_m;j++) {
+				new_arr[i][j]=arr[i][j+ch_m];
+				new_arr[i+ch_n][j]=arr[i][j];
+			}
+			for(int j=ch_m;j<m;j++) {
+				new_arr[i][j]=arr[i+ch_n][j];
+				new_arr[i+ch_n][j]=arr[i+ch_n][j-ch_m];
+			}
+		}
+		return new_arr;
+	}
+}
