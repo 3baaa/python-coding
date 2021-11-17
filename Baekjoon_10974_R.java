fcdb8eada240e0395c4b7a6fb6c1b429cc798dd1
b3aaa <rodrlehqk@naver.com>
2021-11-17 19:36:08 +0900

    모든 순열

---
+++ Baekjoon_10974_R.java
@@ -0,0 +1,61 @@
+package upload.new_upload;
+
+import java.util.*;
+
+/*
+ * 모든 순열
+ * 답 작성후 다시 풀었다
+ */
+public class Baekjoon_10974_R {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for(int i=0;i<n;i++) {
+			a[i] = i+1;
+		}
+		
+		while(true) {
+			for(int i=0;i<n;i++) {
+				System.out.print(a[i]+" ");
+			}
+			System.out.println();
+			if(!next_permutation(a)) {
+				break;
+			}
+		}
+	}
+
+	private static boolean next_permutation(int[] a) {
+		int i = a.length-1;
+		while(i>0 && a[i-1]>=a[i]) {
+			i-=1;
+		}
+		
+		if(i<=0) {
+			return false;
+		}
+		
+		int j = a.length-1;
+		while(a[i-1]>=a[j]) {
+			j-=1;
+		}
+		
+		int temp = a[i-1];
+		a[i-1] = a[j];
+		a[j] = temp;
+		
+		j = a.length-1;
+		while(i<j) {
+			temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+			i++;
+			j--;
+		}
+		
+		return true;
+	}
+
+}
